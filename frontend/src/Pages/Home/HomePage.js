d48ac3ae6e57f8db5195df4b8120ab1ed435f824
devanshi gupta jpg <guptadevanshi044@gmail.com>
2025-06-29 22:35:06 +0530

    commit

--- frontend/src/Pages/Home/HomePage.js
+++ frontend/src/Pages/Home/HomePage.js
@@ -10,9 +10,11 @@ function HomePage() {
       <img src="images/background.jpg" alt="background image" style={{width:"50%",height:"100%"}}/>
      </div>
      <div style={{paddingLeft: 700, paddingTop: 200, fontSize: 20}}>
+
       <div style={{textAlign: "center"}}>
         <h1>Repair Smarter</h1>
       </div>
+      
       <div style={{paddingRight: 200, fontSize: 20,}}>
         <p>Achieve peak efficiency with our advanced defence repair tracker.
         <p>Strealine operations and reduce downtime with ease.</p>
